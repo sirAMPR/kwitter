e5c1b0e85e482b3459f9eac6f774bdbb10ee343f
Taylor Hurt <taylor.a.hurt@gmail.com>
2019-11-15 11:20:30 -0500

    added p tags to display username and displayName

--- src/react/pages/Profile.js
+++ src/react/pages/Profile.js
@@ -2,12 +2,24 @@ import React from "react";
 import { Menu } from "../components";
 import { userIsAuthenticated } from "../HOCs";
 
+const fakeUser = {
+  username: "BobJones",
+  displayName: "Mr. Bob Jones",
+  about: "string",
+  createdAt: "2019-11-15T16:09:13.282Z",
+  updatedAt: "2019-11-15T16:09:13.282Z",
+  pictureLocation: "string",
+  googleId: "string"
+};
+
 class Profile extends React.Component {
   render() {
     return (
       <>
         <Menu isAuthenticated={this.props.isAuthenticated} />
         <h2>Profile</h2>
+        <p>{fakeUser.username}</p>
+        <p>{fakeUser.displayName}</p>
       </>
     );
   }
