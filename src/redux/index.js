fafe7b91da835f802e4904f9f285b31e8aab60cf
Drew Radcliff <sirampr@gmail.com>
2020-03-06 11:50:55 -0500

    add user reducers to store

--- src/redux/index.js
+++ src/redux/index.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import { reducers as authReducers } from "./auth";
+import { reducers as userReducers } from "./users";
 
 export * from "./auth";
 
@@ -13,7 +14,8 @@ export const history = createBrowserHistory({
 export const store = configureStore({
   reducer: {
     router: connectRouter(history),
-    auth: combineReducers(authReducers)
+    auth: combineReducers(authReducers),
+    user: combineReducers(userReducers)
   },
   preloadedState: {},
   devTools: process.env.NODE_ENV !== "production"
