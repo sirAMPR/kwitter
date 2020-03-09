9648e6f14e3bc4d5e56c38644e4530b7961d3bf3
Gabriel Zapata <zapatgab400@gmail.com>
2020-03-09 13:42:40 -0400

    sckelleten is done :v

---
+++ src/react/components/ProfileCard.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import "./Menu.css";
+// import { }
+
+class ProfileCard extends React.Component {
+    
+    render() {
+        return (
+            <div > puto
+            </div>
+            
+            );
+        }
+    }
+    
+    export default ProfileCard
