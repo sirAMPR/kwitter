c1ada4017fb8a85827f5e0836475c386c1e9f4c3
Gabriel Zapata <zapatgab400@gmail.com>
2020-03-11 14:37:31 -0400

    somting on it

--- src/react/components/ProfileCard.js
+++ src/react/components/ProfileCard.js
@@ -1,16 +1,29 @@
 import React from "react";
-import "./Menu.css";
-// import { }
+import "./ProfileCard.css";
+// import { Card, Icon, Image } from 'react';
 
 class ProfileCard extends React.Component {
     
     render() {
         return (
-            <div > puto
-            </div>
             
+            <div className="card">
+                <h1 className="pic">pic</h1>
+                <div className="middle" >
+                    <h1 className="name"> name</h1>
+                    <h3 className="create">date it was creaded</h3>
+                    <p className="bio">bio /hi</p>
+                </div>
+                <div>
+                    <button className="delete" on onClick="-----">DELETE</button>
+                    </div>
+               </div>
+       
             );
         }
     }
     
     export default ProfileCard
+   
+  
+  
