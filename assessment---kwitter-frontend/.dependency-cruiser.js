1fd95b6619e83cd470ab9e4739c79c0017ef3042
Drew Radcliff <sirampr@gmail.com>
2020-03-12 11:05:03 -0400

    remove copy of repo

--- assessment---kwitter-frontend/.dependency-cruiser.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  options: {
-    prefix:
-      "https://gitlab.com/kenzie-academy/se/fe/final-projects/assessment---kwitter-frontend/-/blob/master/"
-  }
-};
--- assessment---kwitter-frontend/src/react/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as LoginForm }
-from "./LoginForm";
-export { default as Menu }
-from "./Menu";
-export { default as ProfileCard }
-from "./ProfileCard"
