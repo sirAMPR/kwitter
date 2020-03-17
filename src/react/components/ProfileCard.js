eaec0aab62bfc79feeb7c79c6ff08b75dfab742f
Drew Radcliff <sirampr@gmail.com>
2020-03-17 10:51:20 -0400

    add user info to profile card

--- src/react/components/ProfileCard.js
+++ src/react/components/ProfileCard.js
@@ -2,71 +2,61 @@ import React from "react";
 import { connect } from "react-redux";
 import { getUser, setProfilePic } from "../../redux";
 import { domain } from "../../redux/helpers";
-import { Card, Image } from "semantic-ui-react"
-
-const fakeUser = {
-  pictureLocation: null,
-  username: "benmckenzie",
-  displayName: "ben",
-  about: "hello, its me",
-  googleId: null,
-  createdAt: "2020-03-10T14:41:55.829Z",
-  updatedAt: "2020-03-10T14:41:55.829Z"
-};
+import { Card, Image } from "semantic-ui-react";
 
 class ProfileCard extends React.Component {
+  state = {
+    pictureLocation: "",
+    username: "",
+    displayName: "",
+    about: "",
+    googleId: "",
+    createdAt: "",
+    updatedAt: ""
+  };
+
   componentDidMount = () => {
     this.props.getUser().then(val =>
       this.setState({
+        pictureLocation: val.payload.user.pictureLocation,
+        username: val.payload.user.username,
         displayName: val.payload.user.displayName,
-        pictureLocation: val.payload.user.pictureLocation
+        about: val.payload.user.about,
+        googleId: val.payload.user.googleId,
+        createdAt: val.payload.user.createdAt,
+        updatedAt: val.payload.user.updatedAt
       })
     );
   };
-  state = {
-    displayName: "",
-    pictureLocation: ""
-  };
-render() {
-  return(
-  <Card>
-    <Image src={domain + this.state.pictureLocation} wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>{fakeUser.username}</Card.Header>
-      <Card.Meta>
-<span className='date'>{fakeUser.displayName}</span>
-      </Card.Meta>
-      <Card.Description>
-        {fakeUser.about}
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-    <p> Created{fakeUser.createdAt}</p>
-   <p> Last Updated:{fakeUser.updatedAt} </p>
-    </Card.Content>
-  </Card>
-  )
- }
+
+  render() {
+    const createDate = new Date(this.state.createdAt);
+    const updateDate = new Date(this.state.updatedAt);
+
+    return (
+      <Card>
+        <Image src={domain + this.state.pictureLocation} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{this.state.displayName}</Card.Header>
+          <Card.Meta>
+            <span className="date">{this.state.username}</span>
+          </Card.Meta>
+          <Card.Description>{this.state.about}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <p> Joined: {createDate.toDateString()}</p>
+          <p> Last Updated: {updateDate.toDateString()} </p>
+        </Card.Content>
+      </Card>
+    );
+  }
 }
-
-
-
-
-  // render() {
-  //   return (
-  //     <>
-  //       <div>{this.state.displayName}</div>
-  //       <img alt="profile-avatar" src={domain + this.state.pictureLocation}></img>
-  //     </>
-  //   );
-  // }
-
 
 export default connect(
   state => ({
-      result: state.user.getUser.result,
-      loading: state.user.getUser.loading,
-      error: state.user.getUser.error
+    result: state.user.getUser.result,
+    loading: state.user.getUser.loading,
+    error: state.user.getUser.error
   }),
   { getUser }
 )(ProfileCard);
