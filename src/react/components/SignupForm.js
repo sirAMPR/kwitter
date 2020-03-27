626290862c5c3aea4524f5ff776e767afcb0d367
Drew Radcliff <sirampr@gmail.com>
2020-03-27 10:11:33 -0400

    add signup error

--- src/react/components/SignupForm.js
+++ src/react/components/SignupForm.js
@@ -21,7 +21,9 @@ class SignupForm extends React.Component {
   handleSignup = e => {
     e.preventDefault();
     this.props.createUser(this.state);
-    return this.props.history.push("/");
+    if (this.props.error === null) {
+      return this.props.history.push("/");
+    }
   };
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -75,4 +77,11 @@ class SignupForm extends React.Component {
   }
 }
 
-export default connect(null, { createUser })(withRouter(SignupForm));
+export default connect(
+  state => ({
+    result: state.user.createUser.result,
+    loading: state.user.createUser.loading,
+    error: state.user.createUser.error
+  }),
+  { createUser }
+)(withRouter(SignupForm));
